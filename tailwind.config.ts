ee7efedcffa8a3b0d2105f8b9b3c916dbbebd665
Matheus Mantini <engmatheusmantini@gmail.com>
2025-02-24 11:25:33 -0300

    style(tailwind): adiciona padrões relacionados ao design do figma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,16 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-poppins)", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "light-gray": "#E3E6E9",
+        "medium-gray": "#8C8C8C",
+        "neutral-gray": "#616161",
+        "dark-gray": "#4E4E4E",
+        primary: "#32C0C6",
+        "primary-light": "rgba(50, 192, 198, 0.7)",
       },
     },
   },
