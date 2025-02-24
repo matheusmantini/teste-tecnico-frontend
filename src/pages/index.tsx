766a8dba7778deab8d29113b0d22b9f14b6320eb
Matheus Mantini <engmatheusmantini@gmail.com>
2025-02-24 11:45:55 -0300

    feat(home): adiciona consulta a api do github via SSR do NextJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import EmptyStateDesktop from "@/components/EmptyStateDesktop";
 import EmptyStateMobile from "@/components/EmptyStateMobile";
+import { getGithubColors } from "@/services/getGithubColors";
+import { getGithubReposData } from "@/services/getGithubReposData";
+import { getGithubUserData } from "@/services/getGithubUserData";
+import { GithubReposColors, iRepo, iUserData } from "@/types";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+interface iProps {
+  colors: GithubReposColors;
+  userResponse?: iUserData;
+  reposResponse?: iRepo[];
+  error?: string;
+}
+
+const Home: React.FC<iProps> = ({
+  colors,
+  userResponse,
+  reposResponse,
+  error,
+}) => {
   return (
     <main className="bg-white" role="main">
       <div className="mx-auto">
@@ -11,3 +28,39 @@ export default function Home() {
     </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { username } = context.query;
+
+  try {
+    const userData = username
+      ? await getGithubUserData(username as string)
+      : null;
+
+    const [reposData, colorsData] = userData
+      ? await Promise.all([
+          getGithubReposData(username as string),
+          getGithubColors(),
+        ])
+      : [null, null];
+
+    return {
+      props: {
+        colors: colorsData || [],
+        userResponse: userData || null,
+        reposResponse: reposData || null,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        colors: [],
+        userResponse: null,
+        reposResponse: null,
+        error: "Erro ao buscar dados. Tente novamente mais tarde.",
+      },
+    };
+  }
+};
+
+export default Home;
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,46 @@ import { ReactNode } from "react";
 export interface iLayoutProps {
   children: ReactNode;
 }
+
+export interface iRepo {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string;
+  language: string;
+  updated_at: string;
+}
+
+export type GithubRepoColor = {
+  color: string;
+  url: string;
+};
+
+export type GithubReposColors = {
+  [repo: string]: GithubRepoColor;
+};
+
+export interface iRepoListProps {
+  colors: GithubReposColors;
+  repos: iRepo[];
+}
+
+export interface iRepoDataProps {
+  repos: iRepo[];
+}
+
+export interface iRepoItemProps {
+  repo: iRepo;
+  colors: GithubReposColors;
+}
+
+export interface iUserData {
+  avatar_url: string;
+  name: string;
+  login: string;
+  bio: string;
+}
+
+export interface iUserProfileProps {
+  userData: iUserData;
+}
